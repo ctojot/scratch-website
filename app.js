440efb2d037396836036aad36619d55c579156c0
ChristianRhey Tojot <christiantojot@gmail.com>
2023-04-21 02:15:42 +0000

    finished website

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 'use strict';
 
-const usersAnswer = prompt("Have you watched anime?");
-console.log(usersAnswer);
+function getAnswer() {
+    const userAnswer = prompt("Have you watched anime before?");
 
-if(usersAnswer == "yes"){
-    document.write("Sweet! Glad to have you here!\n");
-} else if (usersAnswer == "Yes"){
-    document.write("Sweet! Glad to have you here!\n");
-} if (usersAnswer == "no"){
-    document.write("Thats okay! Glad to have you here!\n");
-} if (usersAnswer == "No"){
-    document.write("Thats okay! Glad to have you here!\n");
+    return userAnswer;
+}
+
+function specialMessage(usersAnswer) {
+    while (answer.toLowerCase() != 'yes')
+        if (usersAnswer == "") {
+            usersAnswer = prompt("That's weird. Let's try again. Have you watched anime before?");
+        }
+        if (usersAnswer == "yes") {
+        document.write("Sweet! Glad to have you here!");
+         }
+        if (usersAnswer == "Yes") {
+        document.write("Sweet! Glad to have you here!");
+        }
+        if (usersAnswer == "no") {
+        document.write("Thats okay! Glad to have you here!");
+        }
+        if (usersAnswer == "No") {
+        document.write("Thats okay! Glad to have you here!");
+        }
 }
 
 const date = new Date();
@@ -18,4 +30,11 @@ let day = date.getDate();
 let month = date.getMonth() + 1;
 let year = date.getFullYear();
 document.write("<br>");
-document.write(new Date());
+
+function rateMyPage(){
+    let rating = prompt("How was my website? 1-5? 5 being the best.")
+
+    for (let i = 0; i < rating; i++){
+        document.write("<img src='spongebob.png' alt='spogebob with his hand in his wallet'/>")
+    }
+}
